50a608e8a53cfdac3f57bb06f165a16cccb1d1ec
Mario Perez <perezdhoz@gmail.com>
2021-03-14 12:47:28 -0500

    Update views.py file. All classes inherit from ModelViewSet.

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+# api-small-business/stores/views.py
+from rest_framework import viewsets
+from . import serializers
+from . import models
 
-# Create your views here.
+
+class CityViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing cities.
+    """
+    queryset = models.City.objects.all()
+    serializer_class = serializers.CitySerializer
+    
+    
+class DistrictViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing districts.
+    """
+    queryset = models.District.objects.all()
+    serializer_class = serializers.DistrictSerializer
+    
+    
+class PointOfSaleViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing points of sale.
+    """
+    queryset = models.PointOfSale.objects.all()
+    serializer_class = serializers.PointOfSaleSerializer
+    
+    
+class SellerViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing sellers.
+    """
+    queryset = models.Seller.objects.all()
+    serializer_class = serializers.SellerSerializer
+    
+    
+class ProductViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing products.
+    """
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+    
+    
+class CustomerViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing customers.
+    """
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerSerializer
+    
+    
+class OrderViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing orders.
+    """
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+    
+    
+class OrderDetailViewSet(viewsets.ModelViewSet):
+    """ 
+    This class inherit from ModelViewSet and enable viewing and editing order details.
+    """
+    queryset = models.OrderDetail.objects.all()
+    serializer_class = serializers.OrderDetailSerializer
