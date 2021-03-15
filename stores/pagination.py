5b4b51760ac8362d36a498a42359397f8de671f4
Mario Perez <perezdhoz@gmail.com>
2021-03-15 16:38:34 -0500

    Adding the pagination.py file. extending and overriding the LimitOffsetPagination class.

---
+++ stores/pagination.py
@@ -0,0 +1,7 @@
+# api-small-business/stores/pagination.py
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class HeaderLimitOffsetPagination(LimitOffsetPagination):
+    # Overrides the value assigned to the max_limit class attribute with 10
+    max_limit = 10 
